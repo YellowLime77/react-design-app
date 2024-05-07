d496e185cfde2c2cac844e6477d1775638ab3c91
Matthew <matthewyanglele@gmail.com>
2024-05-07 11:09:52 -0400

    special button completed

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Button.css';
+import '../App.css';
+
+class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            type: "special"
+        }
+    }
+
+    render() {
+        return (
+            <button id="mainButton" className={"button " + this.state.type}>
+                {this.props.text}
+            </button>
+        );
+    }
+}
+
+export default Button;
